042c16073517f36d66f2f952fba0996d1d545068
Sara Powell <newtcobell@gmail.com>
2017-10-01 11:44:24 -0400

    Initial project setup

---
+++ bookswap/your_local_settings.py
@@ -0,0 +1,9 @@
+# To generate a secret key you can use locally, run the following commands in your Python console:
+
+# >>> import random
+# >>> ''.join(random.SystemRandom().choice('abcdefghijklmnopqrstuvwxyz0123456789!@#$%^&*(-_=+)') for i in range(50))
+# save this new key below, in between the quotes
+# change the name of this file to local_settings.py so that .gitignore will not push it to the repo
+# make sure to remove the # from the front of the next line after you add your secret key
+
+# secret_key = 'your generated secret key here!'
