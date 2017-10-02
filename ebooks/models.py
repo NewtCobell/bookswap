7db3c63884e0c9d921a1606aafbda3390bce13d8
Sara Powell <newtcobell@gmail.com>
2017-10-01 20:13:26 -0400

    Extended User model to provide for additional profile fields

--- ebooks/models.py
+++ ebooks/models.py
@@ -1,6 +1,9 @@
 from __future__ import unicode_literals
 
+from django.contrib.auth.models import User
 from django.db import models
+from django.db.models.signals import post_save
+from django.dispatch import receiver
 
 
 class Book(models.Model):
@@ -11,3 +14,20 @@ class Book(models.Model):
     added_on = models.DateTimeField()
     available_date = models.DateField()
 
+
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    first_name = models.CharField(max_length=255)
+    last_name = models.CharField(max_length=255)
+    email = models.CharField(max_length=255)
+
+
+@receiver(post_save, sender=User)
+def create_user_profile(sender, instance, created, **kwargs):
+    if created:
+        Profile.objects.create(user=instance)
+
+
+@receiver(post_save, sender=User)
+def save_user_profile(sender, instance, **kwargs):
+    instance.profile.save()
