24ce9edd4ebdd5ad58d3f442ff4d061463458ac1
Sara Powell <newtcobell@gmail.com>
2017-10-01 20:46:21 -0400

    Refactored user since it turns out the profile items I need are already included in the base user object - doh! Wired in a placeholder index.html page.

--- ebooks/models.py
+++ ebooks/models.py
@@ -2,32 +2,13 @@ from __future__ import unicode_literals
 
 from django.contrib.auth.models import User
 from django.db import models
-from django.db.models.signals import post_save
-from django.dispatch import receiver
 
 
 class Book(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
     title = models.CharField(max_length=255)
     author = models.CharField(max_length=255)
     link = models.CharField(max_length=255)
     deleted = models.BooleanField()
     added_on = models.DateTimeField()
     available_date = models.DateField()
-
-
-class Profile(models.Model):
-    user = models.OneToOneField(User, on_delete=models.CASCADE)
-    first_name = models.CharField(max_length=255)
-    last_name = models.CharField(max_length=255)
-    email = models.CharField(max_length=255)
-
-
-@receiver(post_save, sender=User)
-def create_user_profile(sender, instance, created, **kwargs):
-    if created:
-        Profile.objects.create(user=instance)
-
-
-@receiver(post_save, sender=User)
-def save_user_profile(sender, instance, **kwargs):
-    instance.profile.save()
