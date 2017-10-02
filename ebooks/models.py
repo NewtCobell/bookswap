f21153d67e90c5a8695bb5b7637a45db7e298267
Sara Powell <newtcobell@gmail.com>
2017-10-01 20:01:36 -0400

    Stubbed out book model, added favicon

--- ebooks/models.py
+++ ebooks/models.py
@@ -2,4 +2,12 @@ from __future__ import unicode_literals
 
 from django.db import models
 
-# Create your models here.
+
+class Book(models.Model):
+    title = models.CharField(max_length=255)
+    author = models.CharField(max_length=255)
+    link = models.CharField(max_length=255)
+    deleted = models.BooleanField()
+    added_on = models.DateTimeField()
+    available_date = models.DateField()
+
